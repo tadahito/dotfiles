f2deeb905c147e4727860f2c4fee56f72894428e
tadanohito <yashiropost@gmail.com>
2018-05-28 13:35:18 +0900

    25ver対応

--- emacs/inits/01_package_arv.el
+++ emacs/inits/01_package_arv.el
@@ -4,13 +4,12 @@
 ;; --------------------------------------------------
 ;; ELPA/Marmalade/MELPAパッケージの設定
 (when (require 'package nil t)
-  (add-to-list 'package-archives '("marmalade" . "http://marmalade-repo.org/packages/"))
+  ;; (add-to-list 'package-archives '("marmalade" . "http://marmalade-repo.org/packages/"))
   (add-to-list 'package-archives '("melpa-stable" . "https://stable.melpa.org/packages/") t)
-  (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/"))
+  ;; (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/"))
   (add-to-list 'package-archives '("gnu" . "http://elpa.gnu.org/packages/"))
   (add-to-list 'package-archives '("org" . "http://orgmode.org/elpa/"))
   (setq url-http-attempt-keepalives nil)) ; To fix MELPA problem.
 
-;; 通信時間かかる時は、下記2行コメントアウト
+;; 通信時間かかる時は、下記コメントアウト
 (package-refresh-contents)
-(package-initialize)
