fc3e9a5350078eb5ad00cd4315af69cd68392bc2
tadanohito <yashiropost@gmail.com>
2018-12-02 17:13:43 +0900

    gotest 入れてみる

--- emacs/inits/20_gomode.el
+++ emacs/inits/20_gomode.el
@@ -1,3 +1,23 @@
 ;; go設定---------------------------------------------->
+;; 前提
+;; go get github.com/rogpeppe/godef
+;; go get -u github.com/nsf/gocode
+;; go get github.com/golang/lint/golint
+;; go get github.com/kisielk/errcheck
+;; go get golang.org/x/tools/cmd/goimports
+
 (autoload 'go-mode "go-mode" nil t)
 (add-to-list 'auto-mode-alist '("\\.go\\'" . go-mode))
+
+;; gofmt
+(add-hook 'before-save-hook 'gofmt-before-save)
+
+;; goimport
+(setq gofmt-command "goimports")
+
+;; gotest
+(require 'gotest)
+(setq go-test-verbose t)
+(define-key go-mode-map (kbd "C-c C-t") 'go-test-current-file)
+(define-key go-mode-map (kbd "C-c t") 'go-test-current-test)
+(push '("\*Go Test\*" :regexp t :height 0.5 :stick t) popwin:special-display-config)
